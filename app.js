5b1c3a59a5cefab7447367c2efda1402d3871b20
leyl96 <leidy31valero@gmail.com>
2023-06-08 17:19:46 -0500

    proyecto mercadoliebre

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const path=require('path');
+
+const app= express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/home', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+app.listen(3000, function() {
+    console.log("Servidor corriendo")
+})
